d56eded2957cbd1a62d8f9f84b9ad17dbd6092de
sangeon22 <tkddjsdl33@hanmail.net>
2022-04-25 22:29:57 +0900

    Task 기반 배치, Chunk 기반 배치 실습 commit

---
+++ src/main/java/com/example/spring/batch/part3/ChunkProcessingConfiguration.java
@@ -0,0 +1,113 @@
+package com.example.spring.batch.part3;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@Slf4j
+public class ChunkProcessingConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    public ChunkProcessingConfiguration(JobBuilderFactory jobBuilderFactory,
+                                        StepBuilderFactory stepBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+    /*
+
+    결과를 보면,
+    task 기반은 item size가 100으로 한번에 찍히는 반면에
+    chunk 기반은 item size, list size가 10개씩 10번 찍힌다.
+    chunkSize를 10으로 설정했기 때문에 배치 크기를 일정크기로 반복해서 처리한다.
+
+    물론 task 기반 작업도 chunk처럼 나눠서 처리할 수 있지만, 양이 방대해지고 chunk 기반이 더 잘 구성되어 있다.
+
+     */
+
+    @Bean
+    public Job chunkProcessingJob(){
+        return jobBuilderFactory.get("chunkProcessingJob")
+                .incrementer(new RunIdIncrementer())
+                .start(this.taskBaseStep())
+                .next(this.chunkBaseStep())
+                .build();
+    }
+
+    // chunk 기반
+    // Reader에서 Null을 return할 때 까지 Step이 반복된다.
+    @Bean
+    public Step chunkBaseStep(){
+        return stepBuilderFactory.get("chunkBaseStep")
+                // 100개 데이터를 10개씩 나눠서 처리
+                // <Reader의 input타입, Process의 output타입>으로 반환
+                .<String, String>chunk(10)
+                .reader(itemReader())
+                .processor(itemProcessor())
+                .writer(itemWriter())
+                .build();
+    }
+
+
+    // 아이템을 읽는다.
+    private ItemReader<String> itemReader() {
+        // ListItemReader는 Java에서 제공 들어가서 명세를 봐보자
+        return new ListItemReader<>(getItems());
+    }
+
+    // Reader에서 생성한 데이터를 가공하거나 Writer로 넘길지 결정
+    // 만약에 null로 리턴이 되면, 해당 아이템은 Writer로 넘어가지 못한다.
+    private ItemProcessor<String, String> itemProcessor() {
+        return item -> item + ", Spring Batch";
+    }
+
+    // itemReader에서 읽은 100개의 문자열 리스트 아이템들을 Processor를 거쳐 가공되고 itemWriter에서 로그로 size를 찍는다.
+    private ItemWriter<String> itemWriter() {
+        // itemProcessor를 가공한 결과 확인
+//       return items -> items.forEach(log::info);
+
+        return items -> log.info("chunk item size : {}", items.size());
+    }
+
+
+
+    // task 기반
+    @Bean
+    public Step taskBaseStep() {
+        return stepBuilderFactory.get("taskBaseStep")
+                .tasklet(this.tasklet())
+                .build();
+    }
+
+    private Tasklet tasklet() {
+        return (contribution, chunkContext) -> {
+            List<String> items = getItems();
+            log.info("task item size : {}", items.size());
+            return RepeatStatus.FINISHED;
+        };
+    }
+
+    private List<String> getItems() {
+        List<String> items = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            items.add(i + " Hello");
+        }
+        return items;
+    }
+}
