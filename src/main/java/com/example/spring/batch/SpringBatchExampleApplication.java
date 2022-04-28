cf9a5c2706d48caffdf6f8b2e1ea287840a9d13a
sangeon22 <tkddjsdl33@hanmail.net>
2022-04-28 16:02:53 +0900

    JPA 데이터 쓰기

--- src/main/java/com/example/spring/batch/SpringBatchExampleApplication.java
+++ src/main/java/com/example/spring/batch/SpringBatchExampleApplication.java
@@ -3,14 +3,18 @@ package com.example.spring.batch;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Arrays;
 @SpringBootApplication
 @EnableBatchProcessing
 public class SpringBatchExampleApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(SpringBatchExampleApplication.class, args);
+        System.exit(SpringApplication.exit(SpringApplication.run(SpringBatchExampleApplication.class, args)));
     }
-
 }
+
+
+
+
 
---
+++ src/main/java/com/example/spring/batch/part3/ItemWriterConfiguration.java
@@ -0,0 +1,153 @@
+package com.example.spring.batch.part3;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+
+    템플릿 코드
+    save File as Template
+    
+ */
+
+@Slf4j
+@Configuration
+public class ItemWriterConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final DataSource dataSource;
+    private final EntityManagerFactory entityManagerFactory;
+
+    public ItemWriterConfiguration(JobBuilderFactory jobBuilderFactory,
+                                   StepBuilderFactory stepBuilderFactory,
+                                   DataSource dataSource,
+                                   EntityManagerFactory entityManagerFactory) {
+
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+        this.dataSource = dataSource;
+        this.entityManagerFactory = entityManagerFactory;
+    }
+
+    @Bean
+    public Job itemWriterJob() throws Exception {
+        return this.jobBuilderFactory.get("itemWriterJob")
+                .incrementer(new RunIdIncrementer())
+                .start(this.csvItemWriterStep())
+//                .next(this.jdbcBatchItemWriterStep())
+                .next(this.jpaItemWriterStep())
+                .build();
+    }
+
+    @Bean
+    public Step csvItemWriterStep() throws Exception {
+        return this.stepBuilderFactory.get("csvItemWriterStep")
+                .<Person, Person>chunk(10)
+                .reader(itemReader())
+//                .processor()
+                .writer(csvFileItemWriter())
+                .build();
+    }
+
+    @Bean
+    public Step jdbcBatchItemWriterStep(){
+        return stepBuilderFactory.get("jdbcBatchItemWriterStep")
+                .<Person, Person>chunk(10)
+                .reader(itemReader())
+                .writer(jdbcBatchItemWriter())
+                .build();
+    }
+
+    @Bean
+    public Step jpaItemWriterStep() throws Exception {
+        return stepBuilderFactory.get("JpaItemWriterStep")
+                .<Person, Person>chunk(10)
+                .reader(itemReader())
+                .writer(jpaItemWriter())
+                .build();
+    }
+
+    private ItemWriter<Person> jpaItemWriter() throws Exception {
+        JpaItemWriter<Person> itemWriter = new JpaItemWriterBuilder<Person>()
+                .entityManagerFactory(entityManagerFactory)
+                // select를 통해 수정대상인지 파악하기 때문에 select를 없애기위해
+                .usePersist(true)
+                .build();
+        itemWriter.afterPropertiesSet();
+
+        return itemWriter;
+    }
+
+    private ItemWriter<Person> jdbcBatchItemWriter() {
+        JdbcBatchItemWriter<Person> itemWriter = new JdbcBatchItemWriterBuilder<Person>()
+                .dataSource(dataSource)
+                // person 클래스를 파리미터로 자동으로 생성할 수 있는 프로바이더를 설정
+                .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+                .sql("insert into person(name, age, address) values(:name, :age, :address)")
+                .build();
+        itemWriter.afterPropertiesSet();
+
+        return itemWriter;
+    }
+
+    private ItemWriter<Person> csvFileItemWriter() throws Exception {
+        BeanWrapperFieldExtractor<Person> fieldExtractor = new BeanWrapperFieldExtractor<>();
+        fieldExtractor.setNames(new String[]{"id", "name", "age", "address"});
+
+        DelimitedLineAggregator<Person> lineAggregator = new DelimitedLineAggregator<>();
+        lineAggregator.setDelimiter(",");
+        lineAggregator.setFieldExtractor(fieldExtractor);
+
+        FlatFileItemWriter<Person> itemWriter = new FlatFileItemWriterBuilder<Person>()
+                .name("csvFileItemWriter")
+                .encoding("UTF-8")
+                .resource(new FileSystemResource("output/test-output.csv"))
+                .lineAggregator(lineAggregator)
+                .headerCallback(writer -> writer.write("id,이름,나이,거주지"))
+                .footerCallback(writer -> writer.write("-----------------\n"))
+                // 덮어씌기가 아닌 붙혀쓰기 + footer 끝에 개행문자 추가해야함
+                .append(true)
+                .build();
+        itemWriter.afterPropertiesSet();
+
+        System.out.println();
+        return itemWriter;
+    }
+
+    private ItemReader<Person> itemReader() {
+        return new CustomItemReader<>(getItems());
+    }
+
+    private List<Person> getItems() {
+        List<Person> items = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+//            items.add(new Person(i + 1, "test name "+i, "test age", "test address"));
+            items.add(new Person("test name "+i, "test age", "test address"));
+        }
+        return items;
+    }
+}
